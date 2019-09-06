aaba7360a14fbace2576e2a2e45ae8412fd03881
Allan Ramos <allanramos.teixeira@gmail.com>
2019-09-06 15:06:56 -0300

    Create flutterEnv-win-installer.bat

---
+++ flutterEnv-win-installer.bat
@@ -0,0 +1,13 @@
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco install dart-sdk git
+cd \
+mkdir src
+cd \src
+git clone https://github.com/flutter/flutter.git
+cd flutter
+start bin/flutter.bat doctor
+start flutter_console.bat
+@echo off
+@echo done
+@echo.
+pause
