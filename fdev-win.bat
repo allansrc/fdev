f75ae6b872b5e7a03188638dfc86c2dd817e094f
Allan Ramos <allanramos.teixeira@gmail.com>
2019-11-26 20:38:25 -0300

    Update fdev-win.bat

--- fdev-win.bat
+++ fdev-win.bat
@@ -1,9 +1,9 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-choco install dart-sdk androidstudio git
+choco install git androidstudio
 cd \
 mkdir src
 cd \src
-git clone https://github.com/flutter/flutter.git
+git clone https://github.com/flutter/flutter
 cd flutter
 start bin/flutter.bat doctor
 start flutter_console.bat
